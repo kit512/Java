21b1fbe08f268cd11291175e30e38b33d4f9761c
kit512 <balki94@gmail.com>
2019-01-28 09:24:37 +0300

    HomeWork5

---
+++ Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Person(String name, String position, String email, String phone, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void getInfo(){
+        System.out.println("Name: " + name + "; Position: " + position + "; email: " + email + ";" +
+                " Phone: " + phone + "; Salary: " + salary + "; Age: " + age + ".");
+    }
+
+}
